3c2109f6ba20272309a8717ced3ff1314784fa97
ChernenkiyNikitych4732 <chernenkiy.nikitychsssr@gmail.com>
2023-08-22 13:51:13 +0200

    2 Курс ДЗ №24 Spring изначальный комментарий

---
+++ src/main/java/sky/pro/course2homework/webshop/Kurs2DomasneeZadanie24SpringApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.course2homework.webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs2DomasneeZadanie24SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kurs2DomasneeZadanie24SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/course2homework/webshop/Kurs2DomasneeZadanie24SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.course2homework.webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kurs2DomasneeZadanie24SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
